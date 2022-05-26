a3b8bd2010b80626a112b603d73d594aa70be662
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-26 20:57:55 +0300

    добавление интерфейса именованной сущности

---
+++ Common/MyNotes.Domain.Base/Enitities/INamedEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace MyNotes.Domain.Base.Enitities
+{
+    /// <summary>Именованная сущность</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+    public interface INamedEntity<TKey> : IEntity<TKey>
+    {
+        /// <summary>Имя</summary>
+        string Name { get; set; }
+    }
+
+    /// <summary>Именованная сущность</summary>
+    public interface INamedEntity : INamedEntity<int>, IEntity { }
+}
