43bc5d3af399acf221462e490b7f2127177c5027
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-26 21:08:04 +0300

    сущность записи

---
+++ Common/MyNotes.Domain.Base/INote.cs
@@ -0,0 +1,13 @@
+﻿using MyNotes.Domain.Base.Enitities;
+
+namespace MyNotes.Domain.Base
+{
+    /// <summary>Сущность записи</summary>
+    /// <typeparam name="TKey">Тип первичного ключа</typeparam>
+    public interface INote<TKey> : IEntity<TKey>
+    {
+        string Title { get; set; }
+        string Body { get; set; }
+        DateTime CreationTime { get; set; }
+    }
+}
