4d5f8c7f5bb292266eb95b19b6be89c7f75fee22
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-26 21:13:42 +0300

    добавление новых свойств и комментариев в интерфейс сущности пользователя

--- Common/MyNotes.Domain.Base/INote.cs
+++ Common/MyNotes.Domain.Base/INote.cs
@@ -6,8 +6,16 @@ namespace MyNotes.Domain.Base
     /// <typeparam name="TKey">Тип первичного ключа</typeparam>
     public interface INote<TKey> : IEntity<TKey>
     {
+        /// <summary>Заголовок</summary>
         string Title { get; set; }
+
+        /// <summary>Тело</summary>
         string Body { get; set; }
-        DateTime CreationTime { get; set; }
+
+        /// <summary>Автор</summary>
+        IUser<TKey> Author { get; set; }
+
+        /// <summary>Время создания</summary>
+        DateTime CreationTime { get; }
     }
 }
