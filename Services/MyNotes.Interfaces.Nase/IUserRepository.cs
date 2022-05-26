930bf7ff1c61ba8c48cafe62006e083f7256ed5e
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-26 22:15:21 +0300

    рефакторинг интерфейса репозитория сущностей пользователя

--- Services/MyNotes.Interfaces.Nase/IUserRepository.cs
+++ Services/MyNotes.Interfaces.Nase/IUserRepository.cs
@@ -3,6 +3,12 @@ using MyNotes.Interfaces.Base.Repositories;
 
 namespace MyNotes.Interfaces.Base
 {
-    public interface IUserRepository<T, in TKey> : INamedRepository<T, TKey> where T : IUser<TKey>{ }
+    /// <summary>Репозиторий сущностей пользователя</summary>
+    /// <typeparam name="T">Тип сущности</typeparam>
+    /// <typeparam name="TKey">Тип первичного ключа сущности</typeparam>
+    public interface IUserRepository<T, in TKey> : INamedRepository<T, TKey> where T : IUser<TKey> { }
+
+    /// <summary>Репозиторий сущностей пользователя</summary>
+    /// <typeparam name="T">Тип сущности</typeparam>
     public interface IUserRepository<T> : INamedRepository<T> where T : IUser { }
 }
