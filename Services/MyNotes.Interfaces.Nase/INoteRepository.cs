5915855867b90a39cf81598e6b9b62cc70005bfb
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-28 17:21:35 +0300

    добавление в репозиторий методов для работы с записями

--- Services/MyNotes.Interfaces.Nase/INoteRepository.cs
+++ Services/MyNotes.Interfaces.Nase/INoteRepository.cs
@@ -41,6 +41,24 @@ namespace MyNotes.Interfaces.Base
         /// <returns>Удалённая из репозитория запись в случае её наличия и null, если такой записи в репозитории не было</returns>
         Task<T> DeleteByBody(string Body, CancellationToken Cancel = default);
 
+        /// <summary>Проверка - существует ли в репозитории запись с указанным автором</summary>
+        /// <param name="Author">Автор записи</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Истина, если запись с указанными автором существует в репозитории</returns>
+        Task<bool> ExistAuthor(IUser Author, CancellationToken Cancel = default);
+
+        /// <summary>Получить запись по указанному автору</summary>
+        /// <param name="Author">Автор записи, которую требуется получить из репозитория</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Запись с указанным автором в случае её наличия, и null, если записи с заданным телом в репозитории нет</returns>
+        Task<T> GetByAuthor(IUser Author, CancellationToken Cancel = default);
+
+        /// <summary>Удаление записи с указанным автором из репозитория</summary>
+        /// <param name="Author">Автор удаляемой записи</param>
+        /// <param name="Cancel">Признак отмены асинхронной операции</param>
+        /// <returns>Удалённая из репозитория запись в случае её наличия и null, если такой записи в репозитории не было</returns>
+        Task<T> DeleteByAuthor(IUser Author, CancellationToken Cancel = default);
+
     }
     public interface INoteRepository<T> : INoteRepository<T, int>, IRepository<T> where T : INote { }
 }
