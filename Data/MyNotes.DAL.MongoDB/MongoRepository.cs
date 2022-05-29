537dfac44d571ceb54be77b10444a44ddeffd782
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-29 17:20:48 +0300

    абстрактный класс доступа к коллекции монго

---
+++ Data/MyNotes.DAL.MongoDB/MongoRepository.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Driver;
+
+namespace MyNotes.DAL.MongoDB
+{
+    public abstract class MongoRepository<T>
+    {
+        protected IMongoCollection<T> _col;
+        public MongoRepository(MongoDB db, string collectionName)
+        {
+            _col = db.GetCollection<T>(collectionName);
+        }
+    }
+}
