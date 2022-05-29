15940008e33058a4a6343da87b5074a5b2faf12d
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-29 17:02:14 +0300

    создание абстрактного класса со строкой базы данных

---
+++ Data/MyNotes.DAL/MyNotesDB.cs
@@ -0,0 +1,12 @@
+﻿namespace MyNotes.DAL
+{
+    public abstract class MyNotesDB
+    {
+        protected string ConnectionString { get; set; }
+
+        public MyNotesDB(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+    }
+}
