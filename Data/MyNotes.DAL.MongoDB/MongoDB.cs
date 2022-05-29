058226548b446950c8a3c8636bda8eadf9bddfae
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-29 17:15:14 +0300

    класс подключения к МонгоДБ

---
+++ Data/MyNotes.DAL.MongoDB/MongoDB.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+
+namespace MyNotes.DAL.MongoDB
+{
+    public class MongoDB : MyNotesDB
+    {
+        IMongoDatabase _db;
+        IMongoClient _client;
+        public MongoDB (string databaseName, string connectionString) : base(connectionString)
+        {
+            _client = new MongoClient (connectionString);
+            _db = _client.GetDatabase(databaseName);
+        }
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            return _db.GetCollection<T>(name);
+        }
+    }
+}
