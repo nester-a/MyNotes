fcc035b24aa54e6dd4d455c2bb6c07298ce27525
Алексей Нестеров <nester.a.l@yandex.ru>
2022-05-29 17:52:00 +0300

    методы расширения для работы с БД монго

---
+++ Data/MyNotes.DAL.MongoDB/Extensions/MongoCollectionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using MyNotes.Domain.Base.Enitities;
+
+namespace MyNotes.DAL.MongoDB.Extensions
+{
+    public static class MongoCollectionExtensions
+    {
+        public static bool Contains<T>(this IMongoCollection<T> collection, T obj) where T : class, IEntity<string>
+        {
+            if (string.IsNullOrWhiteSpace(obj.Id)) return false;
+            var findResult = collection.Find(new BsonDocument("_id", new ObjectId(obj.Id)));
+            var result = findResult.ToList();
+            if (result.Count == 0)
+                return false;
+            return true;
+        }
+    }
+}
